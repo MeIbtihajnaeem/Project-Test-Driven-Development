243d96b4bf4f202f1e99658d69c867492147b291
ibtihajnaeem <i.ibtihajnaeem@gmail.com>
2024-04-13 15:09:39 +0200

    Red: testNameMethodWithSpecialCharacters

--- com.mycompany.orderAssignmentSystem/src/main/java/com/mycompany/orderAssignmentSystem/controller/utils/ValidationConfigurations.java
+++ com.mycompany.orderAssignmentSystem/src/main/java/com/mycompany/orderAssignmentSystem/controller/utils/ValidationConfigurations.java
@@ -4,11 +4,11 @@ public class ValidationConfigurations {
 
 	public String validateName(String name) {
 		if (name == null || name == "") {
-			throw new NullPointerException("The name field cannot be empty");
+			throw new NullPointerException("The name field cannot be empty.");
 		}
 		if (name.length() <= 2) {
 			throw new IllegalArgumentException(
-					"The name must be at least 3 characters long. Please provide a valid name");
+					"The name must be at least 3 characters long. Please provide a valid name.");
 		}
 		if (name.length() > 20) {
 			throw new IllegalArgumentException("The name cannot exceed 20 characters. Please provide a shorter name.");
--- com.mycompany.orderAssignmentSystem/src/test/java/com/mycompany/orderAssignmentSystem/controller/utils/ValidationConfigurationsTest.java
+++ com.mycompany.orderAssignmentSystem/src/test/java/com/mycompany/orderAssignmentSystem/controller/utils/ValidationConfigurationsTest.java
@@ -10,14 +10,14 @@ public class ValidationConfigurationsTest {
 	public void testNameMethodWithNullString() {
 		ValidationConfigurations validationConfigurations = new ValidationConfigurations();
 		assertThatThrownBy(() -> validationConfigurations.validateName(null)).isInstanceOf(NullPointerException.class)
-				.hasMessage("The name field cannot be empty");
+				.hasMessage("The name field cannot be empty.");
 	}
 
 	@Test
 	public void testNameMethodWithEmptyString() {
 		ValidationConfigurations validationConfigurations = new ValidationConfigurations();
 		assertThatThrownBy(() -> validationConfigurations.validateName("")).isInstanceOf(NullPointerException.class)
-				.hasMessage("The name field cannot be empty");
+				.hasMessage("The name field cannot be empty.");
 	}
 
 	@Test
@@ -27,7 +27,7 @@ public class ValidationConfigurationsTest {
 			String name = "a";
 			validationConfigurations.validateName(name);
 		}).isInstanceOf(IllegalArgumentException.class)
-				.hasMessage("The name must be at least 3 characters long. Please provide a valid name");
+				.hasMessage("The name must be at least 3 characters long. Please provide a valid name.");
 	}
 
 	@Test
@@ -37,7 +37,7 @@ public class ValidationConfigurationsTest {
 			String name = "ab";
 			validationConfigurations.validateName(name);
 		}).isInstanceOf(IllegalArgumentException.class)
-				.hasMessage("The name must be at least 3 characters long. Please provide a valid name");
+				.hasMessage("The name must be at least 3 characters long. Please provide a valid name.");
 	}
 	
 	@Test
@@ -49,5 +49,17 @@ public class ValidationConfigurationsTest {
 		}).isInstanceOf(IllegalArgumentException.class)
 				.hasMessage("The name cannot exceed 20 characters. Please provide a shorter name.");
 	}
+	
+	@Test
+	public void testNameMethodWithSpecialCharacters() {
+		ValidationConfigurations validationConfigurations = new ValidationConfigurations();
+		assertThatThrownBy(() -> {
+			String name = "aaaaa@";
+			validationConfigurations.validateName(name);
+		}).isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("The name cannot contain special characters. Please remove any special characters from the name.");
+	}
+	
+
 
 }
