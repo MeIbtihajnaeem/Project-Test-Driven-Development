acf2231b5b1eb63ab651e9644dacc7c377e30602
ibtihajnaeem <i.ibtihajnaeem@gmail.com>
2024-04-13 16:11:45 +0200

    Green: testPhoneNumberMethodWithEndingWhiteSpaceCharachters

--- com.mycompany.orderAssignmentSystem/src/test/java/com/mycompany/orderAssignmentSystem/controller/utils/ValidationConfigurationsForValidatePhoneNumberMethodTest.java
+++ com.mycompany.orderAssignmentSystem/src/test/java/com/mycompany/orderAssignmentSystem/controller/utils/ValidationConfigurationsForValidatePhoneNumberMethodTest.java
@@ -87,4 +87,13 @@ public class ValidationConfigurationsForValidatePhoneNumberMethodTest {
 				"The phone number should only consist of numbers and should not contain any whitespaces, special characters, or alphabets. Please enter a valid phone number.");
 	}
 
+	@Test
+	public void testPhoneNumberMethodWithEndingWhiteSpaceCharachters() {
+		String phoneNumber = "340137207 ";
+		assertThatThrownBy(() -> {
+			validationConfigurations.validatePhoneNumber(phoneNumber);
+		}).isInstanceOf(IllegalArgumentException.class).hasMessage(
+				"The phone number should only consist of numbers and should not contain any whitespaces, special characters, or alphabets. Please enter a valid phone number.");
+	}
+
 }
