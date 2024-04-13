1201b0352e7e88af14a8de8d360f7fb8d4edeec2
ibtihajnaeem <i.ibtihajnaeem@gmail.com>
2024-04-13 15:13:57 +0200

    Refactor: testNameMethodWithSpecialCharacters

--- com.mycompany.orderAssignmentSystem/src/main/java/com/mycompany/orderAssignmentSystem/controller/utils/ValidationConfigurations.java
+++ com.mycompany.orderAssignmentSystem/src/main/java/com/mycompany/orderAssignmentSystem/controller/utils/ValidationConfigurations.java
@@ -16,13 +16,18 @@ public class ValidationConfigurations {
 		if (name.length() > 20) {
 			throw new IllegalArgumentException("The name cannot exceed 20 characters. Please provide a shorter name.");
 		}
-		Pattern pattern = Pattern.compile("[^a-zA-Z0-9\\s]");
-		Matcher matcher = pattern.matcher(name);
-		if (matcher.find()) {
+
+		if (_containsSpecialCharacters(name)) {
 			throw new IllegalArgumentException(
 					"The name cannot contain special characters. Please remove any special characters from the name.");
 		}
 		return null;
+	}
+
+	private boolean _containsSpecialCharacters(String str) {
+		Pattern pattern = Pattern.compile("[^a-zA-Z0-9\\s]");
+		Matcher matcher = pattern.matcher(str);
+		return matcher.find();
 	}
 
 }
