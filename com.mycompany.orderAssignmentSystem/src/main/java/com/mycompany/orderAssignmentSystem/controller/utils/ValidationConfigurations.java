d3d16a4c8979545acf4cd675c5651750d21458fb
ibtihajnaeem <i.ibtihajnaeem@gmail.com>
2024-04-13 14:06:08 +0200

    Refactor: testNameMethodWithEmptyString

--- com.mycompany.orderAssignmentSystem/src/main/java/com/mycompany/orderAssignmentSystem/controller/utils/ValidationConfigurations.java
+++ com.mycompany.orderAssignmentSystem/src/main/java/com/mycompany/orderAssignmentSystem/controller/utils/ValidationConfigurations.java
@@ -3,10 +3,7 @@ package com.mycompany.orderAssignmentSystem.controller.utils;
 public class ValidationConfigurations {
 
 	public String validateName(String name) {
-		if (name == null) {
-			throw new NullPointerException("The name field cannot be empty");
-		}
-		if (name == "") {
+		if (name == null || name == "") {
 			throw new NullPointerException("The name field cannot be empty");
 		}
 		return null;
