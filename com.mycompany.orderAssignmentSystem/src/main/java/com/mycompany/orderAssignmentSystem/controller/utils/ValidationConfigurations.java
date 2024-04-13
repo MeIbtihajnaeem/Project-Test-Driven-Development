7a033d9a731ee0c537a00de7c2e022c05af39015
ibtihajnaeem <i.ibtihajnaeem@gmail.com>
2024-04-13 14:09:34 +0200

    Green: testNameMethodWithShortString

--- com.mycompany.orderAssignmentSystem/src/main/java/com/mycompany/orderAssignmentSystem/controller/utils/ValidationConfigurations.java
+++ com.mycompany.orderAssignmentSystem/src/main/java/com/mycompany/orderAssignmentSystem/controller/utils/ValidationConfigurations.java
@@ -6,6 +6,10 @@ public class ValidationConfigurations {
 		if (name == null || name == "") {
 			throw new NullPointerException("The name field cannot be empty");
 		}
+		if (name.length() <= 2) {
+			throw new IllegalArgumentException(
+					"The name must be at least 3 characters long. Please provide a valid name");
+		}
 		return null;
 	}
 
