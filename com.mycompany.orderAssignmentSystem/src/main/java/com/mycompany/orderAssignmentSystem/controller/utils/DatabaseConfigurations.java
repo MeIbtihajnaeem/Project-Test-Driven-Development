31a2413e662443bffa5eff48597840b912cf237d
ibtihajnaeem <i.ibtihajnaeem@gmail.com>
2024-06-19 19:08:28 +0200

    Fixed unit test

--- com.mycompany.orderAssignmentSystem/src/main/java/com/mycompany/orderAssignmentSystem/controller/utils/DatabaseConfigurations.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.orderAssignmentSystem.controller.utils;
-
-import javax.persistence.EntityManager;
-
-public interface DatabaseConfigurations {
-
-	public EntityManager getConnection();
-}
