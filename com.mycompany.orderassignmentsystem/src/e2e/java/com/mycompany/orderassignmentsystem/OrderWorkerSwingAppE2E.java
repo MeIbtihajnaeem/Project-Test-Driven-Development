8781ebc14603486449ce0ef0eb18d198c5cb9483
ibtihajnaeem <i.ibtihajnaeem@gmail.com>
2024-06-21 23:06:42 +0200

    Renamed e2e tests because of failures

--- com.mycompany.orderassignmentsystem/src/e2e/java/com/mycompany/orderassignmentsystem/OrderWorkerSwingAppE2E.java
+++ com.mycompany.orderassignmentsystem/src/e2e/java/com/mycompany/orderassignmentsystem/OrderWorkerSwingAppE2E.java
@@ -30,7 +30,7 @@ import com.mycompany.orderassignmentsystem.model.CustomerOrder;
 import com.mycompany.orderassignmentsystem.model.Worker;
 
 @RunWith(GUITestRunner.class)
-public class OrderWorkerSwingAppEndToEndTest extends AssertJSwingJUnitTestCase {
+public class OrderWorkerSwingAppE2E extends AssertJSwingJUnitTestCase {
 	private static final String PERSISTENCE_UNIT_NAME = "OriginalPersistenceUnit";
 	private static final int MAX_RETRIES = 3;
 	private static final long RETRY_DELAY_SECONDS = 10;
