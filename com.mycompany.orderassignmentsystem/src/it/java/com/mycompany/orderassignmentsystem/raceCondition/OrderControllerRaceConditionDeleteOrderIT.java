1d392f297390665188371ef5e9d09139203e4ef0
ibtihajnaeem <i.ibtihajnaeem@gmail.com>
2024-06-22 21:23:13 +0200

    Added Race conditions for integration tests

--- com.mycompany.orderassignmentsystem/src/it/java/com/mycompany/orderassignmentsystem/raceCondition/OrderControllerRaceConditionDeleteOrderIT.java
+++ com.mycompany.orderassignmentsystem/src/it/java/com/mycompany/orderassignmentsystem/raceCondition/OrderControllerRaceConditionDeleteOrderIT.java
@@ -1,96 +1,120 @@
-//package com.mycompany.orderassignmentsystem.raceCondition;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.awaitility.Awaitility.await;
-//
-//import java.util.List;
-//import java.util.concurrent.TimeUnit;
-//import java.util.stream.Collectors;
-//import java.util.stream.IntStream;
-//
-//import javax.persistence.EntityManagerFactory;
-//import javax.persistence.Persistence;
-//
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//
-//import com.mycompany.orderAssignmentSystem.controller.OrderController;
-//import com.mycompany.orderAssignmentSystem.controller.utils.ValidationConfigurations;
-//import com.mycompany.orderAssignmentSystem.controller.utils.extensions.ExtendedValidationConfigurations;
-//import com.mycompany.orderAssignmentSystem.enumerations.OrderCategory;
-//import com.mycompany.orderAssignmentSystem.enumerations.OrderStatus;
-//import com.mycompany.orderAssignmentSystem.model.CustomerOrder;
-//import com.mycompany.orderAssignmentSystem.model.Worker;
-//import com.mycompany.orderAssignmentSystem.repository.OrderRepository;
-//import com.mycompany.orderAssignmentSystem.repository.WorkerRepository;
-//import com.mycompany.orderAssignmentSystem.repository.postgres.OrderDatabaseRepository;
-//import com.mycompany.orderAssignmentSystem.repository.postgres.WorkerDatabaseRepository;
-//import com.mycompany.orderAssignmentSystem.view.OrderView;
-//
-//public class OrderControllerRaceConditionDeleteOrderIT {
-//
-//	private OrderRepository orderRepository;
-//
-//	@Mock
-//	private OrderView orderView;
-//
-//	private WorkerRepository workerRepository;
-//
-//	private ValidationConfigurations validationConfig;
-//
-//	private AutoCloseable closeable;
-//
-//	private EntityManagerFactory entityManagerFactory;
-////	private EntityManager entityManager;
-//	private static final String PERSISTENCE_UNIT_NAME = "OriginalPersistenceUnit";
-//	private Worker worker = new Worker();
-//	private CustomerOrder savedOrder = new CustomerOrder();
-//
-//	@Before
-//	public void setUp() {
-//		closeable = MockitoAnnotations.openMocks(this);
-//		entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-////		entityManager = entityManagerFactory.createEntityManager();
-//
-//		orderRepository = new OrderDatabaseRepository(entityManagerFactory);
-//		workerRepository = new WorkerDatabaseRepository(entityManagerFactory);
-//		validationConfig = new ExtendedValidationConfigurations();
-////		orderController = new OrderController(orderRepository, orderView, workerRepository, validationConfig);
-//		worker.setWorkerName("Jhon");
-//		worker.setWorkerCategory(OrderCategory.PLUMBER);
-//		worker = workerRepository.save(worker);
-//		CustomerOrder order = new CustomerOrder("Jhon", "Piazza Luigi Dalla", "3401372678", "12-12-2024",
-//				"No description", OrderCategory.PLUMBER, OrderStatus.PENDING, worker);
-//		savedOrder = orderRepository.save(order);
-//	}
-//
-//	@After
-//	public void releaseMocks() throws Exception {
-////		entityManager.clear();
-//
-//		entityManagerFactory.close();
-////		entityManager.close();
-//
-//		closeable.close();
-//	}
-//
-//	@Test
-//	public void deleteOrderConcurrent() {
-//
-//		List<Thread> threads = IntStream.range(0, 3).mapToObj(i -> new Thread(() -> {
-//			try {
-//				new OrderController(orderRepository, orderView, workerRepository, validationConfig)
-//						.deleteOrder(savedOrder);
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//		})).peek(t -> t.start()).collect(Collectors.toList());
-//		await().atMost(10, TimeUnit.SECONDS).until(() -> threads.stream().noneMatch(t -> t.isAlive()));
-//
-//		assertThat(orderRepository.findAll()).isEmpty();
-//		;
-//	}
-//}
+package com.mycompany.orderassignmentsystem.raceCondition;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.mycompany.orderassignmentsystem.controller.OrderController;
+import com.mycompany.orderassignmentsystem.controller.utils.ValidationConfigurations;
+import com.mycompany.orderassignmentsystem.controller.utils.extensions.ExtendedValidationConfigurations;
+import com.mycompany.orderassignmentsystem.enumerations.OrderCategory;
+import com.mycompany.orderassignmentsystem.enumerations.OrderStatus;
+import com.mycompany.orderassignmentsystem.model.CustomerOrder;
+import com.mycompany.orderassignmentsystem.model.Worker;
+import com.mycompany.orderassignmentsystem.repository.OrderRepository;
+import com.mycompany.orderassignmentsystem.repository.WorkerRepository;
+import com.mycompany.orderassignmentsystem.repository.postgres.OrderDatabaseRepository;
+import com.mycompany.orderassignmentsystem.repository.postgres.WorkerDatabaseRepository;
+import com.mycompany.orderassignmentsystem.view.OrderView;
+
+public class OrderControllerRaceConditionDeleteOrderIT {
+	private static final String PERSISTENCE_UNIT_NAME = "OriginalPersistenceUnit";
+	private static final int MAX_RETRIES = 3;
+	private static final long RETRY_DELAY_SECONDS = 10;
+
+	@BeforeClass
+	public static void setup() {
+
+		int attempt = 0;
+		while (attempt < MAX_RETRIES) {
+			try {
+				EntityManagerFactory entityManagerFactory = Persistence
+						.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+
+				EntityManager entityManager = entityManagerFactory.createEntityManager();
+				if (entityManager != null && entityManager.isOpen()) {
+					entityManager.close();
+					break;
+				}
+			} catch (Exception i) {
+				attempt++;
+				if (attempt < MAX_RETRIES) {
+					try {
+						TimeUnit.SECONDS.sleep(RETRY_DELAY_SECONDS);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+		}
+	}
+
+	private OrderRepository orderRepository;
+
+	@Mock
+	private OrderView orderView;
+
+	private WorkerRepository workerRepository;
+
+	private ValidationConfigurations validationConfig;
+
+	private AutoCloseable closeable;
+
+	private EntityManagerFactory entityManagerFactory;
+	private Worker worker = new Worker();
+	private CustomerOrder savedOrder = new CustomerOrder();
+
+	@Before
+	public void setUp() {
+		closeable = MockitoAnnotations.openMocks(this);
+		entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		orderRepository = new OrderDatabaseRepository(entityManagerFactory);
+		workerRepository = new WorkerDatabaseRepository(entityManagerFactory);
+		validationConfig = new ExtendedValidationConfigurations();
+		worker.setWorkerName("Jhon");
+		worker.setWorkerCategory(OrderCategory.PLUMBER);
+		worker = workerRepository.save(worker);
+		CustomerOrder order = new CustomerOrder("Jhon", "Piazza Luigi Dalla", "3401372678", "12-12-2024",
+				"No description", OrderCategory.PLUMBER, OrderStatus.PENDING, worker);
+		savedOrder = orderRepository.save(order);
+	}
+
+	@After
+	public void releaseMocks() throws Exception {
+		entityManagerFactory.close();
+		closeable.close();
+	}
+
+	@Test
+	public void deleteOrderConcurrent() {
+
+		List<Thread> threads = IntStream.range(0, 10).mapToObj(i -> new Thread(() -> {
+			try {
+				new OrderController(orderRepository, orderView, workerRepository, validationConfig)
+						.deleteOrder(savedOrder);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		})).peek(t -> t.start()).collect(Collectors.toList());
+		await().atMost(10, TimeUnit.SECONDS).until(() -> threads.stream().noneMatch(t -> t.isAlive()));
+
+		assertThat(orderRepository.findAll()).isEmpty();
+		;
+	}
+}
